b5d9b3b89a7f3503ecdfcf2c4f6ef815fd95711d
imranq2 <imranq2@hotmail.com>
2021-11-20 14:17:30 -0800

    add dependencies on spark

---
+++ src/com/bwell/AddNumber.java
@@ -0,0 +1,10 @@
+package com.bwell;
+
+import org.apache.spark.sql.api.java.UDF1;
+public class AddNumber implements UDF1<Long, Long> {
+    private static final long serialVersionUID = 1L;
+    @Override
+    public Long call(Long num) throws Exception {
+        return (num + 5);
+    }
+}
