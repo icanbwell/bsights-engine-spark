1206deac434a6c680224867a47fd72a1613066e4
imranq2 <imranq2@hotmail.com>
2021-11-21 15:36:22 -0800

    fix unit test

--- src/test/java/com/bwell/RunCqlTest.java
+++ src/test/java/com/bwell/RunCqlTest.java
@@ -1,20 +1,17 @@
 package com.bwell;
 
 import com.holdenkarau.spark.testing.SharedJavaSparkContext;
-import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.*;
 import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.MapType;
-import org.apache.spark.sql.types.StringType;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import com.google.common.collect.Lists;
 
-import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RunCqlTest extends SharedJavaSparkContext {
@@ -22,18 +19,20 @@ public class RunCqlTest extends SharedJavaSparkContext {
     @BeforeClass
     public void setup() {
         this.runBefore();
+        jsc().setLogLevel("WARN");
     }
 
     @Test
     public void testRunCqlUdf() {
-        List<String> data = new ArrayList<String>();
-        data.add("green");
-        data.add("red");
-        JavaRDD<String> jsonRDD = jsc().parallelize(data);
         SQLContext sqlContext = new SQLContext(jsc());
         sqlContext.sparkSession().udf().register("runCql", new RunCql(), DataTypes.createMapType(DataTypes.StringType, DataTypes.StringType));
 
-        Dataset<Row> df = sqlContext.read().json(jsonRDD);
+        List<Row> rows = Arrays.asList(RowFactory.create("green"), RowFactory.create("red"));
+        StructType schema = DataTypes.createStructType(
+                new StructField[]{DataTypes.createStructField("color", DataTypes.StringType, false)});
+
+        Dataset<Row> df = sqlContext.createDataFrame(rows, schema);
+        df.show();
         df.createOrReplaceTempView("numbersdata");
         Dataset<Row> result_df = sqlContext.sql("SELECT runCql('cqlLibraryUrl', 'terminologyUrl', 'patient') As num1 from numbersdata");
         result_df.printSchema();
