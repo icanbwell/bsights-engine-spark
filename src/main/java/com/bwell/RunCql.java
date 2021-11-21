99d5191c2cc22eb9564c8a634e9d6417990ac611
imranq2 <imranq2@hotmail.com>
2021-11-21 14:46:56 -0800

    return Map

--- src/main/java/com/bwell/RunCql.java
+++ src/main/java/com/bwell/RunCql.java
@@ -2,11 +2,15 @@ package com.bwell;
 
 import org.apache.spark.sql.api.java.UDF3;
 
-public class RunCql implements UDF3<String, String, String, String> {
+public class RunCql implements UDF3<String, String, String, java.util.Map<String, String>> {
     private static final long serialVersionUID = 1L;
     @Override
-    public String call(String cqlLibraryUrl, String terminologyUrl, String fhirBundle) throws Exception {
-        return "foo";
+    public java.util.Map<String, String> call(String cqlLibraryUrl, String terminologyUrl, String fhirBundle) throws Exception {
+        java.util.Map<String, String> newMap = new java.util.HashMap<>();
+        newMap.put("key1", "value1");
+        newMap.put("key2", "value2");
+
+        return newMap;
     }
 }
 
