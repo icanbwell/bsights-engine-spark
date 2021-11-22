42d5a5b84cb62801366d251d05fab204d08dc761
imranq2 <imranq2@hotmail.com>
2021-11-22 09:14:37 -0800

    add comments

--- src/main/java/com/bwell/CqlRunner.java
+++ src/main/java/com/bwell/CqlRunner.java
@@ -55,39 +55,51 @@ public class CqlRunner {
     public EvaluationResult runCql(String fhirVersion, List<LibraryParameter> libraries) {
         FhirVersionEnum fhirVersionEnum = FhirVersionEnum.valueOf(fhirVersion);
 
+        // create an evaluator with the passed in fhirVersion
         CqlEvaluatorComponent cqlEvaluatorComponent = DaggerCqlEvaluatorComponent.builder()
                 .fhirContext(fhirVersionEnum.newContext()).build();
 
+        // load cql libraries
         for (LibraryParameter library : libraries) {
 
+            // create a cql evaluator builder
             CqlEvaluatorBuilder cqlEvaluatorBuilder = cqlEvaluatorComponent.createBuilder();
 
+            // check if it is in the cache
             LibraryContentProvider libraryContentProvider = libraryContentProviderIndex.get(library.libraryUrl);
 
+            // if not in cache then load it
             if (libraryContentProvider == null) {
                 libraryContentProvider = cqlEvaluatorComponent.createLibraryContentProviderFactory()
                         .create(new EndpointInfo().setAddress(library.libraryUrl));
+                // put it in cache
                 libraryContentProviderIndex.put(library.libraryUrl, libraryContentProvider);
             }
 
+            // add libraries to cql evaluator builder
             cqlEvaluatorBuilder.withLibraryContentProvider(libraryContentProvider);
 
+            // load terminology
             if (library.terminologyUrl != null) {
+                // check if terminology is in cache
                 TerminologyProvider terminologyProvider = terminologyProviderIndex.get(library.terminologyUrl);
                 if (terminologyProvider == null) {
+                    // if terminology is not in cache then load ut
                     terminologyProvider = cqlEvaluatorComponent.createTerminologyProviderFactory()
                             .create(new EndpointInfo().setAddress(library.terminologyUrl));
+                    // add to cache
                     terminologyProviderIndex.put(library.terminologyUrl, terminologyProvider);
                 }
 
+                // add terminology to cql evaluator builder
                 cqlEvaluatorBuilder.withTerminologyProvider(terminologyProvider);
             }
 
+            // load the data to evaluate
             Triple<String, ModelResolver, RetrieveProvider> dataProvider = null;
             DataProviderFactory dataProviderFactory = cqlEvaluatorComponent.createDataProviderFactory();
-            // check if th model is provided as text then just use that
-
             if (library.model != null) {
+                // if model is provided as text then use it
                 if (library.model.modelText != null){
                     File f = new File("/usr/local/bin/geeks");
                     String resource = null;
@@ -106,8 +118,8 @@ public class CqlRunner {
                     dataProvider = dataProviderFactory.create(bundle);
                 }
                 else {
+                    // load model from url
                     dataProvider = dataProviderFactory.create(new EndpointInfo().setAddress(library.model.modelUrl));
-
                 }
             }
             // default to FHIR
@@ -115,19 +127,23 @@ public class CqlRunner {
                 dataProvider = dataProviderFactory.create(new EndpointInfo().setType(Constants.HL7_FHIR_FILES_CODE));
             }
 
+            // add data to cql evaluator builder
             cqlEvaluatorBuilder.withModelResolverAndRetrieveProvider(dataProvider.getLeft(), dataProvider.getMiddle(),
                     dataProvider.getRight());
 
+            // build the evaluator
             CqlEvaluator evaluator = cqlEvaluatorBuilder.build();
 
             VersionedIdentifier identifier = new VersionedIdentifier().withId(library.libraryName);
 
+            // add any context parameters
             Pair<String, Object> contextParameter = null;
 
             if (library.context != null) {
                 contextParameter = Pair.of(library.context.contextName, library.context.contextValue);
             }
 
+            // run evaluator and return result
             return evaluator.evaluate(identifier, contextParameter);
         }
         return null;
