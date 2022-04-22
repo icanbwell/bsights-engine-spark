9edf8d97a6fba652562cd4e3ea78883d2a86a512
changsu-bWell <changsu.lee@icanbwell.com>
2022-04-22 16:38:31 -0600

    minor updates on Utilities class

--- src/test/java/com/bwell/utilities/Utilities.java
+++ src/test/java/com/bwell/utilities/Utilities.java
@@ -11,7 +11,6 @@ import org.json.JSONObject;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.UUID;
 
 public class Utilities {
 
@@ -50,47 +49,6 @@ public class Utilities {
         }
 
         return rawJson;
-    }
-
-    public static String separateResourcesFromContainedJson(String rawContainedJson) throws JSONException {
-        UUID uuid = UUID.randomUUID();
-        String bundleContainedJson = "[{\"bundle\": "
-                + "{\"resourceType\":\"Bundle\", \"id\":\""
-                + uuid
-                + "\", \"entry\":[" + rawContainedJson + "]}"
-                + "}]";
-
-        JSONArray jsonArray = new JSONArray(bundleContainedJson);
-        JSONObject firstBundle = (JSONObject) jsonArray.get(0);
-
-        JSONObject convertedJsonObj = convertContainedBundleToNormalBundle(firstBundle);
-        return convertedJsonObj.toString();
-    }
-
-    public static JSONObject convertContainedBundleToNormalBundle(JSONObject jsonBundleObject) throws JSONException {
-        JSONArray entryArray = jsonBundleObject.getJSONArray("entry");
-        JSONObject firstElement = (JSONObject) entryArray.get(0);
-
-        JSONObject firstResource = firstElement.getJSONObject("resource");
-        if (firstResource != null) {
-            JSONArray containedArray = firstResource.getJSONArray("contained");
-            if (containedArray.length() > 0) {
-                for(int i=0; i<containedArray.length(); i++) {
-                    JSONObject resourceObj = containedArray.getJSONObject(i);
-                    jsonBundleObject
-                            .getJSONArray("entry")
-                            .put(new JSONObject("{\"resource\":" + resourceObj.toString() + "}"));
-                }
-            }
-        }
-
-        ((JSONObject) jsonBundleObject.getJSONArray("entry").get(0))
-                .getJSONObject("resource")
-                .remove("contained");
-
-        System.out.println(jsonBundleObject);
-
-        return jsonBundleObject;
     }
 
     public static String tempConvert(Object value) {
